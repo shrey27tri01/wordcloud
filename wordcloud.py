f5ed4224ffa2068226023a31f2b31fc394b06f19
shrey27tri01 <shreytripathi27.com>
2020-07-08 16:22:35 +0530

    first commit

---
+++ wordcloud.py
@@ -0,0 +1,91 @@
+# Here are all the installs and imports you will need for your word cloud script and uploader widget
+
+#pip install wordcloud
+#pip install fileupload
+#pip install ipywidgets
+#jupyter nbextension install --py --user fileupload
+#jupyter nbextension enable --py fileupload
+
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+# This is the uploader widget
+def _upload():
+
+    _upload_widget = fileupload.FileUploadWidget()
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+
+_upload()
+
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    
+    # LEARNER CODE START HERE
+    for character in file_contents:
+        if character in punctuations:
+            #print(character)
+            file_contents = file_contents.replace(character, "")
+    #print(file_contents)
+    
+    word_dict = {}
+            
+    words = file_contents.split(" ")
+    #print(words)
+    lower_words = []
+    interesting_words = []
+    
+    for word in words:
+        word = word.lower()
+        lower_words.append(word)
+        #print(word)
+    
+    #print(lower_words)
+    
+    for word in lower_words:
+        if word not in uninteresting_words:
+            #print(word)
+            if word.isalpha():
+                interesting_words.append(word)
+            
+    #print(interesting_words)
+    
+    for word in interesting_words:
+        #print(word)
+        if word in word_dict.keys():
+            word_dict[word] += 1
+        elif word not in word_dict.keys():
+            word_dict[word] = 1
+    
+    #print(word_dict)
+    #wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(word_dict)
+    return cloud.to_array()
+
+# Display your wordcloud image
+
+myimage = calculate_frequencies(file_contents)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()
